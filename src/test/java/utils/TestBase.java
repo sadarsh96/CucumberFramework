d0abc16feedefa669d0ad95941efebd540c136b2
Adarsh <sadarsh96@gmail.com>
2022-08-25 11:34:48 +0530

    path modified for global properties

--- src/test/java/utils/TestBase.java
+++ src/test/java/utils/TestBase.java
@@ -18,7 +18,7 @@ public class TestBase {
 	public WebDriver driver;
 	public WebDriver webDriverManager() throws IOException {
 		
-		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\resources\\global.properties");
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"/src/test/resources/global.properties");
 		Properties prop = new Properties();
 		prop.load(fis);
 		String url = prop.getProperty("url");
